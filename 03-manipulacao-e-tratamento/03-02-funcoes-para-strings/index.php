a977a3edddd1cd6a556ed2f62209a6a81e02895a
Caio Moraes <camoredev@gmail.com>
2021-02-04 13:55:56 -0300

    03-manipulacao-e-tratamento/

--- 03-manipulacao-e-tratamento/03-02-funcoes-para-strings/index.php
+++ 03-manipulacao-e-tratamento/03-02-funcoes-para-strings/index.php
@@ -20,7 +20,6 @@ var_dump([
     "strtoupper" => strtoupper($string),
     "mb_strtoupper" => mb_strtoupper($string)
 ]);
-
 echo "</pre>";
 
 
@@ -29,14 +28,71 @@ echo "</pre>";
  */
 fullStackPHPClassSession("conversão de caixa", __LINE__);
 
+$mbString = $string;
+
+echo "<pre>";
+var_dump([
+    "mb_strtiupper" => mb_strtoupper($mbString),
+    "mb_strtolower" => mb_strtolower($mbString),
+    "mb_convert_case UPPER" => mb_convert_case($mbString, MB_CASE_UPPER),
+    "mb_convert_case LOWER" => mb_convert_case($mbString, MB_CASE_LOWER),
+    "mb_convert_case TITLE" => mb_convert_case($mbString, MB_CASE_TITLE)
+]);
+echo "</pre>";
 
 /**
  * [ substituição ] multibyte and replace
  */
 fullStackPHPClassSession("substituição", __LINE__);
 
+$mbReplace = $mbString . " Fui, iria novamente, e foi épico!";
+
+echo "<pre>";
+var_dump([
+   "mb_strlen" => mb_strlen($mbReplace),
+   "mb_strpos" => mb_strpos($mbReplace, ", "),
+   "mb_strrpos" => mb_strrpos($mbReplace, ", "),
+   "mb_substr" => mb_substr($mbReplace, 40 + 2, 14) . "!",
+   "mb_strstr" => mb_strstr($mbReplace, ", ", true),
+   "mb_strtchr" => mb_strrchr($mbReplace, ", ", false)
+]);
+echo "</pre>";
+
+$mbStrReplace = $string;
+
+echo "<p>", $mbStrReplace ,"</p>";
+echo "<p>", str_replace("AC/DC", "Dream theater", $mbStrReplace) ,"</p>";
+echo "<p>", str_replace(["AC/DC", "último"], "Nivana", $mbStrReplace) ,"</p>";
+echo "<p>", str_replace(["AC/DC", "incrível"], ["Dream Theater", "épico!!"], $mbStrReplace) ,"</p>";
+
+
+$article = <<<rock
+    <article>
+        <h3>event</h3>
+        <p>desc</p>
+    </article>
+rock;
+
+$articleData = [
+    "event" => "Rock in Rio",
+    "desc" => $mbStrReplace
+];
+
+echo "<p>", str_replace(array_keys($articleData), array_values($articleData), $article) ,"</p>";
 
 /**
  * [ parse string ] parse_str | mb_parse_str
  */
 fullStackPHPClassSession("parse string", __LINE__);
+
+$endPoint = "name=Caio&email=caio@camoredev.com.br";
+
+mb_parse_str($endPoint, $parseEndPoint);
+
+echo "<pre>";
+var_dump([
+    $endPoint,
+    $parseEndPoint,
+    (object)$parseEndPoint
+]);
+echo "</pre>";
